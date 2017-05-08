36ce80027a4beb5e0e1963816b30dc99c871c35f
Lee McDowell <shadolee@mail.com>
2017-05-08 17:08:47 +1000

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 
 const axios = require('axios');
-// const prompt = require('prompt');
+const prompt = require('prompt');
 
 
-function getWeather() {
-  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&APPID=fc71a4a69bc4a5db42d4b84e5087235e`)
+function getWeather(city) {
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=fc71a4a69bc4a5db42d4b84e5087235e`)
   .then(function (response) {
     console.log('Current temperature:', response.data["main"]["temp"] + " C");
     console.log('Pressure:', response.data["main"]["pressure"]);
     console.log('Wind speed:', response.data["wind"]["speed"]);
-    console.log('Looking:', response.data["weather"]["main"]);
+
 
   })
   .catch(function (error) {
@@ -18,13 +18,11 @@ function getWeather() {
   });
 }
 
-getWeather();
+  console.log('Welcome to the Weather checker');
+  console.log('Please enter a city to check:');
 
-  // console.log('Welcome to the Weather checker');
-  // console.log('Please enter a username to check:');
-  //
-  // prompt.start();
-  //
-  // prompt.get(['username'], function (err, result) {
-  //   getGithub(result.username);
-  // });
+  prompt.start();
+
+  prompt.get(['city'], function (err, result) {
+    getWeather(result.city);
+  });
